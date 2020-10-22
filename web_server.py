ecd6c9f71a901d75f83a76b6c1694fb27dda21ce
Max <max.bergmark@gmail.com>
2020-10-22 16:58:23 +0200

    Added threading lock to prevent race condition

--- web_server.py
+++ web_server.py
@@ -3,6 +3,8 @@ from typing import List
 from pydantic import BaseModel, Field, validator
 from fastapi import FastAPI
 # import multiprocessing
+import threading
+
 
 lib = ctypes.CDLL('./sudoku_solver.so')
 lib.Sudoku_new.restype = ctypes.c_void_p
@@ -20,6 +22,7 @@ class Sudoku(object):
 	def __init__(self):
 
 		self.obj = lib.Sudoku_new()
+		self.lock = threading.Lock()
 		# threads = multiprocessing.cpu_count()
 		# self.obj = lib.ParallelSolver_new(threads)
 
@@ -29,7 +32,13 @@ class Sudoku(object):
 	def solve(self, strs):
 		byte_strs = list(map(lambda s: s.encode("utf-8"), strs))
 		string_pointers = (ctypes.c_char_p * len(byte_strs))(*byte_strs)
-		lib.solve_sudokus(self.obj, string_pointers, len(strs))
+
+		self.lock.acquire()
+		try:
+			lib.solve_sudokus(self.obj, string_pointers, len(strs))
+		finally:
+			self.lock.release()
+
 		# lib.solve_sudokus_parallel(self.obj, string_pointers, len(strs))
 		return list(map(lambda b: b.decode("utf-8"), byte_strs))
 
